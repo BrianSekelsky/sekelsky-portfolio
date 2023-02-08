98c139bfd0cf410b16c48a3809c3888264136136
Brian Sekelsky <bsekels@ncsu.edu>
2023-02-07 23:37:59 -0500

    flip colors

--- src/script.js
+++ src/script.js
@@ -16,7 +16,7 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.render(scene, camera)
 
-scene.background = new THREE.Color( 0xffffff );
+// scene.background = new THREE.Color( 0xffffff );
 scene.fog = new THREE.Fog( 0xffffff, 0.95, 100 ); 
 
 /**
