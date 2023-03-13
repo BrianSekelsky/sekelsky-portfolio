81dfa3860ad82d1a6db542aa04c797a6b95c0be1
Brian Sekelsky <bsekels@ncsu.edu>
2023-03-13 17:49:02 -0400

    bunch of restructuring

--- src/script.js
+++ src/script.js
@@ -24,30 +24,37 @@ const renderer = new THREE.WebGLRenderer({
 renderer.render(scene, camera)
 
 // scene.background = new THREE.Color( 0xffffff );
-scene.background = new THREE.Color( 0xf8fdff );
-scene.fog = new THREE.Fog( 0xf8fdff, 0.95, 40 ); 
+scene.background = new THREE.Color( 0x000000 );
+scene.fog = new THREE.Fog( 0x000000, 0.95, 40 ); 
 
 // Light
 // const light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 2 );
 const light = new THREE.AmbientLight( 0x404040 ); // soft white light
 scene.add( light );
 
+const rotationGroup = new THREE.Group();
+
 /**
  * Particles
  */
 // Geometry
-const particlesGeometry = new THREE.TorusKnotGeometry( 7, 1.5, 200, 32 )
-particlesGeometry.rotateZ(Math.PI / 2)
+// const particlesGeometry = new THREE.TorusKnotGeometry( 7, 1.5, 200, 32 )
+// const particlesGeometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const particlesGeometry = new THREE.SphereGeometry( 15, 64, 64 );
+// const particlesGeometry = new THREE.BoxGeometry( 15, 15, 15, 15, 15, 15 );
+// particlesGeometry.rotateZ(Math.PI / 2)
 // const particlesGeometry = new THREE.BufferGeometry()
-const count = 2000
 
 const particlesMaterial = new THREE.PointsMaterial({
     size: 0.015,
-    color: 0x001f25,
+    color: 0x9da4b3,
 });
+
+// const particlesMaterial = new THREE.MeshBasicMaterial();
+
 // Material
 if(!isMobile){
-    particlesMaterial.size = 0.054
+    particlesMaterial.size = 0.064
 } else {
     particlesMaterial.size = 0.038
 }
@@ -56,18 +63,21 @@ if(!isMobile){
 const particles = new THREE.Points(particlesGeometry, particlesMaterial)
 const particles2 = new THREE.Points(particlesGeometry, particlesMaterial)
 
-scene.add(particles)
+scene.add(rotationGroup)
 
 if(!isMobile){
     // scene.add(particles2)
 }
 
-particles.position.set(0, 2, 0)
+particles.position.set(0, 1.8, 0)
 if(isMobile){
     particles.position.set(2, 4, 3)
 }
 particles2.position.set(0, -32, 0)
 
+particles.scale.set(0.5, 0.5, 0.5)
+
+rotationGroup.add(particles)
 
 /**
  * Parallax
@@ -77,13 +87,15 @@ cursor.x = 0
 cursor.y = 0
 
 window.addEventListener('mousemove', (event) => {
-    cursor.x = event.clientX / 800 - 0.5
-    cursor.y = event.clientY / 600 - 0.5
+    cursor.x = event.clientX / window.innerWidth - 0.5
+    cursor.y = event.clientY / window.innerHeight - 0.5
 })
 
 let matrix = new THREE.Matrix4()
 
 const clock = new THREE.Clock()
+
+console.log(rotationGroup)
 
 const tick = () =>
 {
@@ -112,17 +124,14 @@ const tick = () =>
     // camera.position.x = parallaxX * 1
     // camera.position.y = parallaxY * 2
 
-    console.log(parallaxX)
-    console.log(parallaxY)
+    // camera.lookAt(0,0,0)
 
-    camera.lookAt(0,0,0)
+    rotationGroup.children[0].rotation.x += parallaxY * 0.02
+    rotationGroup.children[0].rotation.y += parallaxX * 0.02
 
-    particles.rotation.y += parallaxX * 0.03
-    particles.rotation.x += parallaxX * 0.03
+    let scale = 0.6 - parallaxY * 0.5
 
-    let scale = 0.5 - parallaxY
-
-    particles.scale.set(scale, scale, scale)
+    // particles.scale.set(scale, scale, scale)
 
     // particles2.rotation.y = parallaxX * 0.5
     // particles2.rotation.x = parallaxY * 0.5
